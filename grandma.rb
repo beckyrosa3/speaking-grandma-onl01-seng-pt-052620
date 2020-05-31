25f8ae29f5aa490e6e7ed2aa7a7284be04dc73d8
Becky Rosa <becky_m_rosa@yahoo.com>
2020-05-31 20:33:02 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,11 @@
 def speak_to_grandma(phrase)
-  if phrase != phrase.upcase
-puts "HUH?! SPEAK UP, SONNY!"
-  elsif phrase == phrase.upcase
-puts "NO, NOT SINCE 1938!"
-  else phrase == "I LOVE YOU GRANDMA!"
-puts "I LOVE YOU TOO PUMPKIN!"
+  "I LOVE YOU TOO PUMPKIN!"
+  if phrase.upcase == 'I LOVE YOU GRANDMA!'
+    "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase != phrase.upcase
+    "HUH?! SPEAK UP, SONNY!"
+  else
+    "NO, NOT SINCE 1938!"
+  end
+
 end
